c752e48c56aaaf434239d333b3798fb28f49ce91
Icahbod <usmanimamu17@gmail.com>
2026-06-14 13:49:36 +0000

    docs: add ApexChain header to payments/page.tsx

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -1,4 +1,5 @@
 import PaymentsView from "@/components/payments/payments-view";
+/** ApexChain Network Operations Intelligence Platform */
 
 export default function PaymentPage() {
     return <PaymentsView />;
